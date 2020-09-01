a93a869f72e44231841671c26ff0afb4dfc17527
Ayush <ayupradhan1999@gmail.com>
2020-09-01 17:44:50 +0530

    Changes in nav and some media queries

--- js/main.js
+++ js/main.js
@@ -10,3 +10,15 @@ $(".topnav a, .btn").on("click", function (event) {
     );
   }
 });
+$(".footer a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 110,
+      },
+      800
+    );
+  }
+});
