38485a21b3e32ae2127a1ce5d2f4362c6af691fe
Ayush <ayupradhan1999@gmail.com>
2020-09-01 19:03:03 +0530

    ..

--- js/main.js
+++ js/main.js
@@ -22,3 +22,15 @@ $(".footer a, .btn").on("click", function (event) {
     );
   }
 });
+// $(".topnav button, .btn").on("click", function (event) {
+//   if (this.hash !== "") {
+//     event.preventDefault();
+//     const hash = this.hash;
+//     $("html, body").animate(
+//       {
+//         scrollTop: $(hash).offset().top - 110,
+//       },
+//       800
+//     );
+//   }
+// });
